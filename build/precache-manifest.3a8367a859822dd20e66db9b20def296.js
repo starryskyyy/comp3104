d11324afbaf7aed31afb57c17a114cd993f16360
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2022-11-13 00:45:27 -0500

    React project code added

---
+++ build/precache-manifest.3a8367a859822dd20e66db9b20def296.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fb5a8f9a73a4cc9ce11",
+    "url": "/starryskyyy/comp3104/static/css/main.3a181c3f.chunk.css"
+  },
+  {
+    "revision": "3fb5a8f9a73a4cc9ce11",
+    "url": "/starryskyyy/comp3104/static/js/main.3fb5a8f9.chunk.js"
+  },
+  {
+    "revision": "c597aa6304aa38f9129d",
+    "url": "/starryskyyy/comp3104/static/js/runtime~main.c597aa63.js"
+  },
+  {
+    "revision": "b1773f8afd9fb6a0106e",
+    "url": "/starryskyyy/comp3104/static/js/2.b1773f8a.chunk.js"
+  },
+  {
+    "revision": "e1c451efde3979ceb05e127c2bc57a92",
+    "url": "/starryskyyy/comp3104/index.html"
+  }
+];
